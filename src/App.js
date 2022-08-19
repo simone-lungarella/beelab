0ff2488f0ce760d2d861782c6724d5c939907373
Simone Lungarella <simonelungarella@gmail.com>
2022-08-19 19:23:52 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import { Box, Grid, IconButton } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import React from 'react';
 import './App.css';
+import Logo from './AppLogo.js';
+import TitlebarImageList from './TitlebarImageList.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AppBar>
+        <Toolbar>
+          <Logo />
+        </Toolbar>
+      </AppBar>
+
+      <Box p={3} />
+
+      <TitlebarImageList />
+
+      <Box p={5} />
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+        }} >
+        <Grid container direction="row" justifyContent="center" alignItems="center">
+          <Grid item>
+            <IconButton size='large' href='https://it-it.facebook.com/'>
+              <FacebookIcon />
+            </IconButton>
+          </Grid>
+          <Grid item>
+            <IconButton size='large' href='https://www.instagram.com/falegnameria_beelab/'>
+              <InstagramIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Box>
+
+    </React.Fragment>
   );
 }
 
---
+++ src/AppLogo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from "./logo.svg";
+import Box from "@mui/material/Box";
+
+function AppLogo() {
+    return (
+        <Box
+            component="img"
+            sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '0%',
+                transform: 'translate(10%, -50%)',
+                height: 64,
+            }}
+
+            alt="App logo"
+            src={Logo}
+        />
+    );
+}
+
+export default AppLogo;
